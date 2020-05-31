785c0fd18ffb8c59bafac2294dd51eb3b275296c
deja68 <jolicic.andrea@gmail.com>
2020-05-31 14:46:26 +0200

    added username to be index, required instead of require, changed email type, added multer package

--- index.js
+++ index.js
@@ -1,178 +1,179 @@
-const express = require('express')
-const { json, urlencoded } = require('body-parser')
+const express = require("express");
+const { json, urlencoded } = require("body-parser");
 
-const User = require('./controllers/users');
-const Product = require('./controllers/products');
-const { connect } = require('./helpers')
-const { DB_URL } = require("./config")
+const User = require("./controllers/users");
+const Product = require("./controllers/products");
+const { connect } = require("./helpers");
+const { DB_URL } = require("./config");
 
-const app = express()
-app.use(urlencoded({ extended: true }))
-app.use(json())
+const app = express();
+app.use(urlencoded({ extended: true }));
+app.use(json());
 
-app.get('/product/:name', async (req, res) => {
-    const productName = req.params.name;
-    try{
-        const product = await Product.findByName(productName);
-        res.status(200).json(product);
-    } catch (error) {
-        res.json(error)
-    }
-})
-app.delete('/product/:name', async (req, res) => {
-    const productName = req.params.name;
-    try{
-        const product = await Product.delete({name:productName});
-        res.status(204).json(product);
-    } catch (error) {
-        res.json(error)
-    }
-})
-app.put('/product/:name', async (req, res) => {
-    const productName = req.params.name;
-    
-    const queryToUpdate = req.body;
-    try{
-        const product = await Product.update({name:productName}, queryToUpdate);
-        res.status(204).json(product);
-    } catch (error) {
-        res.json(error)
-    }
-})
-app.post('/product', async (req, res) => {
-    const productToCreate = req.body;
-    try {
-        const product = await Product.create(productToCreate)
-        res.status(201).json(product)
-    } catch (error) {
-        res.json(error)
-    }
-})
-app.get('/products', async (req, res) => {
-    try{
-        if(Object.keys(req.query).length === 0){
-            const products = await Product.findAll();
-            res.status(200).json(products)
-        }else{
-            const limit = parseInt(req.query.limit);
-            const offset = parseInt(req.query.offset);
+app.get("/product/:name", async (req, res) => {
+  const productName = req.params.name;
+  try {
+    const product = await Product.findByName(productName);
+    res.status(200).json(product);
+  } catch (error) {
+    res.json(error);
+  }
+});
+app.delete("/product/:name", async (req, res) => {
+  const productName = req.params.name;
+  try {
+    const product = await Product.delete({ name: productName });
+    res.status(204).json(product);
+  } catch (error) {
+    res.json(error);
+  }
+});
+app.put("/product/:name", async (req, res) => {
+  const productName = req.params.name;
 
-            const product = await Product.findGroupOfProducts(limit,offset);
-            res.status(200).json(product);
-        }
-        
-    } catch (error) {
-        res.json(error)
-    }
-})
+  const queryToUpdate = req.body;
+  try {
+    const product = await Product.update({ name: productName }, queryToUpdate);
+    res.status(204).json(product);
+  } catch (error) {
+    res.json(error);
+  }
+});
+app.post("/product", async (req, res) => {
+  const productToCreate = req.body;
+  try {
+    const product = await Product.create(productToCreate);
+    res.status(201).json(product);
+  } catch (error) {
+    res.json(error);
+  }
+});
+app.get("/products", async (req, res) => {
+  try {
+    if (Object.keys(req.query).length === 0) {
+      const products = await Product.findAll();
+      res.status(200).json(products);
+    } else {
+      const limit = parseInt(req.query.limit);
+      const offset = parseInt(req.query.offset);
 
-app.get('/user/:username', async (req, res) => {
-    const userName = req.params.username;
-    try{
-        const user = await User.findByName(userName);
-        res.status(200).json(user);
-    } catch (error) {
-        res.json(error)
+      const product = await Product.findGroupOfProducts(limit, offset);
+      res.status(200).json(product);
     }
-})
-app.delete('/user/:username', async (req, res) => {
-    const userName = req.params.username;
-    try{
-        const user = await User.delete({username:userName});
-        res.status(204).json(user);
-    } catch (error) {
-        res.json(error)
-    }
-})
-app.put('/user/:username', async (req, res) => {
-    const userName = req.params.username;
-    
-    const queryToUpdate = req.body;
-    try{
-        const user = await User.update({name:userName}, queryToUpdate);
-        res.status(204).json(user);
-    } catch (error) {
-        res.json(error)
-    }
-})
-app.post('/user', async (req, res) => {
-    const userToCreate = req.body;
-    try {
-        const user = await User.create(userToCreate)
-        res.status(201).json(user)
-    } catch (error) {
-        res.json(error)
-    }
-})
-app.get('/users', async (req, res) => {
-    try{
-        const users = await User.findAll();
-        res.status(200).json(users)
-    } catch (error) {
-        res.json(error)
-    }
-})
+  } catch (error) {
+    res.json(error);
+  }
+});
 
-app.get('/product_id/:id', async (req, res) => {
-    const productId = req.params.id;
-    try{
-        const product = await Product.findById(productId);
-        res.status(200).json(product);
-    } catch (error) {
-        res.json(error)
-    }
-})
-app.delete('/product_id/:id', async (req, res) => {
-    const productId = req.params.id;
-    try{
-        const product = await Product.deleteById(productId);
-        res.status(204).json(product);
-    } catch (error) {
-        res.json(error)
-    }
-})
-app.put('/product_id/:id', async (req, res) => {
-    const productId = req.params.id;
-    
-    const queryToUpdate = req.body;
-    try{
-        const product = await Product.updateById(productId, queryToUpdate);
-        res.status(204).json(product);
-    } catch (error) {
-        res.json(error)
-    }
-})
-app.put('/product_dec/:id', async (req, res) => {
-    const productId = req.params.id;
-    console.log(productId)
-    try{
-        const product = await Product.dec(productId);
-        res.status(204).json(product);
-    } catch (error) {
-        res.json(error)
-    }
-})
-app.put('/product_inc/:id', async (req, res) => {
-    const productId = req.params.id;
-    try{
-        const product = await Product.inc(productId);
-        res.status(204).json(product);
-    } catch (error) {
-        res.json(error)
-    }
-})
-app.get('/product_num/:id', async (req, res) => {
-    const productId = req.params.id;
-    try{
-        const product = await Product.numberOfProducts(productId);
-        res.status(200).json(product);
-    } catch (error) {
-        res.json(error)
-    }
-})
+app.get("/user/:username", async (req, res) => {
+  const userName = req.params.username;
+  try {
+    const user = await User.findByName(userName);
+    res.status(200).json(user);
+  } catch (error) {
+    res.json(error);
+  }
+});
+app.delete("/user/:username", async (req, res) => {
+  const userName = req.params.username;
+  try {
+    const user = await User.delete({ username: userName });
+    res.status(204).json(user);
+  } catch (error) {
+    res.json(error);
+  }
+});
+app.put("/user/:username", async (req, res) => {
+  const userName = req.params.username;
+
+  const queryToUpdate = req.body;
+  try {
+    const user = await User.update({ name: userName }, queryToUpdate);
+    res.status(204).json(user);
+  } catch (error) {
+    res.json(error);
+  }
+});
+app.post("/user", async (req, res) => {
+  const userToCreate = req.body;
+  try {
+    const user = await User.create(userToCreate);
+    res.status(201).json(user);
+  } catch (error) {
+    res.json(error);
+  }
+});
+app.get("/users", async (req, res) => {
+  try {
+    const users = await User.findAll();
+    res.status(200).json(users);
+  } catch (error) {
+    res.json(error);
+  }
+});
+
+app.get("/product_id/:id", async (req, res) => {
+  const productId = req.params.id;
+  try {
+    const product = await Product.findById(productId);
+    res.status(200).json(product);
+  } catch (error) {
+    res.json(error);
+  }
+});
+app.delete("/product_id/:id", async (req, res) => {
+  const productId = req.params.id;
+  try {
+    const product = await Product.deleteById(productId);
+    res.status(204).json(product);
+  } catch (error) {
+    res.json(error);
+  }
+});
+app.put("/product_id/:id", async (req, res) => {
+  const productId = req.params.id;
+
+  const queryToUpdate = req.body;
+  try {
+    const product = await Product.updateById(productId, queryToUpdate);
+    res.status(204).json(product);
+  } catch (error) {
+    res.json(error);
+  }
+});
+app.put("/product_dec/:id", async (req, res) => {
+  const productId = req.params.id;
+  console.log(productId);
+  try {
+    const product = await Product.dec(productId);
+    res.status(204).json(product);
+  } catch (error) {
+    res.json(error);
+  }
+});
+app.put("/product_inc/:id", async (req, res) => {
+  const productId = req.params.id;
+  try {
+    const product = await Product.inc(productId);
+    res.status(204).json(product);
+  } catch (error) {
+    res.json(error);
+  }
+});
+app.get("/product_num/:id", async (req, res) => {
+  const productId = req.params.id;
+  try {
+    const product = await Product.numberOfProducts(productId);
+    res.status(200).json(product);
+  } catch (error) {
+    res.json(error);
+  }
+});
 
 connect(DB_URL)
-  .then(() => app.listen(5000, () => {
-    console.log('server on http://localhost:5000')
-  }))
-  .catch(e => console.error(e))
+  .then(() =>
+    app.listen(5000, () => {
+      console.log("server on http://localhost:5000");
+    })
+  )
+  .catch((e) => console.error(e));
--- models/products.js
+++ models/products.js
@@ -2,55 +2,53 @@ const mongoose = require("mongoose");
 const validate = require("mongoose-validator");
 
 const nameValidator = [
-    validate({
-      validator: 'isLength',
-      arguments: [3, 15],
-      message: 'Name should be between 3 and 15 characters',
-    })
-]
+  validate({
+    validator: "isLength",
+    arguments: [3, 15],
+    message: "Name should be between 3 and 15 characters",
+  }),
+];
 const descriptionValidator = [
-    validate({
-        validator: 'isLength',
-        arguments: [10, 150],
-        message: 'Name should be between 10 and 150 characters',
-      })
-]
+  validate({
+    validator: "isLength",
+    arguments: [10, 150],
+    message: "Name should be between 10 and 150 characters",
+  }),
+];
 
-const product = new mongoose.Schema(
-    {
-        name:{
-            type: String,
-            require: true,
-            unique: true,
-            validate: nameValidator,
-            index: true
-        },
-        description:{
-            type:String,
-            validate: descriptionValidator
-        },
-        image: {
-            type: String,
-        },
-        price:{
-            type: Number,
-            require: true,
-            min: 1,
-            max: 10000
-        },
-        quantity:{
-            type: Number,
-            min: 1,
-            max: 10,
-            default: 1
-        },
-        user:{
-            type: mongoose.Schema.Types.ObjectId,
-            required: true,
-            ref: "user"
-        }
-    }
-)
+const product = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    unique: true,
+    validate: nameValidator,
+    index: true,
+  },
+  description: {
+    type: String,
+    validate: descriptionValidator,
+  },
+  image: {
+    type: String,
+  },
+  price: {
+    type: Number,
+    required: true,
+    min: 1,
+    max: 10000,
+  },
+  quantity: {
+    type: Number,
+    min: 1,
+    max: 10,
+    default: 1,
+  },
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+    ref: "user",
+  },
+});
 
 const ProductModel = mongoose.model("product", product);
-module.exports = ProductModel
+module.exports = ProductModel;
