a66f6f2c00ecd0635eab8eacbf1041c15b1cd802
deja68 <jolicic.andrea@gmail.com>
2020-06-07 23:21:42 +0200

    changed limit for hashed password, changed jwt.verify method

--- auth/check-auth.js
+++ auth/check-auth.js
@@ -3,7 +3,8 @@ const jwt = require("jsonwebtoken");
 module.exports = (req, res, next) => {
   try {
     //verify method returns decoded if succeeds; verify method verifys and decodes token
-    const decoded = jwt.verify(req.body.token, process.env.JWT_KEY);
+    const token = req.headers.authorization.split(" ")[1];
+    const decoded = jwt.verify(token, process.env.JWT_KEY);
     req.userData = decoded;
     next(); //if we successfuly authenticate
   } catch (error) {
